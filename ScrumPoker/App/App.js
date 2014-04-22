9b8c43c215842f3e671b705261743af9266e37cb
Andy Zivkovic <zivkovic.andy@gmail.com>
2014-04-22 21:02:00 +0200

    angularjs basic routing

---
+++ ScrumPoker/App/App.js
@@ -0,0 +1,22 @@
+﻿var scrumPokerApp = angular.module('scrumPokerApp', ['ngRoute', 'scrumPokerControllers']);
+
+scrumPokerApp.config([
+    '$routeProvider', function ($routeProvider) {
+        $routeProvider.when('/', {
+            templateUrl: 'partials/lobby.html',
+            controller: 'lobby'
+        }).
+            when('/room/:room', {
+                templateUrl: 'partials/room.html',
+                controller: 'room'
+            }).
+            otherwise({ redirectTo: '/' });
+    }
+]);
+
+var scrumPokerControllers = angular.module('scrumPokerControllers', []);
+
+scrumPokerControllers.controller('lobby', ['$scope', '$http', function ($scope, $http) {
+    $scope.rooms = [];
+}]);
+scrumPokerControllers.controller('room', ['$scope', function($scope) {}]);
