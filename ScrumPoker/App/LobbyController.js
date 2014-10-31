07de22568447851eac40c98a8b0fd95888df15ef
Andy Zivkovic <zivkovic.andy@gmail.com>
2014-10-31 09:59:06 +0100

    split javascript into separate components

---
+++ ScrumPoker/App/LobbyController.js
@@ -0,0 +1,10 @@
+﻿(function () {
+    'use strict';
+
+    angular.module('scrumPokerControllers').controller('lobby', [
+        '$scope', 'PokerServer', function($scope, server) {
+            $scope.server = server;
+        }
+    ]);
+
+})();
---
+++ ScrumPoker/App/RoomController.js
@@ -0,0 +1,21 @@
+﻿(function() {
+    'use strict';
+
+    angular.module('scrumPokerControllers').controller('room', [
+        '$scope', 'PokerServer', '$routeParams', function($scope, server, $routeParams) {
+            $scope.server = server;
+            $scope.roomId = $routeParams.roomId;
+
+            $scope.$watch('myBet', function(newValue, oldValue) {
+                if (newValue != null) {
+                    if (newValue === '-')
+                        newValue = null;
+                    $scope.server.Bet(newValue);
+                }
+            });
+
+            $scope.allowedBets = [0, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89];
+        }
+    ]);
+
+})();
