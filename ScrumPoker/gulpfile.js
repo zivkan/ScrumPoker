8405b8432144782c20f78c6e670449fa5a11c8b1
Andy Zivkovic <zivkovic.andy@gmail.com>
2015-02-10 01:47:16 +0100

    minify html and js

--- ScrumPoker/gulpfile.js
+++ ScrumPoker/gulpfile.js
@@ -1,23 +1,29 @@
 ﻿var gulp = require("gulp");
 var concat = require("gulp-concat");
 var templateCache = require("gulp-angular-templatecache");
+var minHtml = require("gulp-minify-html");
+var sourcemap = require("gulp-sourcemaps");
+var uglify = require("gulp-uglify");
 
 gulp.task("script", function() {
     gulp.src([
             "App/AppModule.js",
             "App/ControllerModule.js",
-            "App/templates.js",
             "App/ConnectionController.js",
             "App/LobbyController.js",
             "App/PokerServer.js",
             "App/RoomController.js"
-        ])
-        .pipe(concat("app.js"))
+    ])
+        .pipe(sourcemap.init())
+            .pipe(concat("app.js"))
+            .pipe(uglify())
+        .pipe(sourcemap.write("."))
         .pipe(gulp.dest("."));
 });
 
 gulp.task("templates", function() {
     gulp.src("partials/*.html")
-        .pipe(templateCache("templates.js", { standalone: true, root:"partials/" }))
+        .pipe(minHtml())
+        .pipe(templateCache("templates.js", { standalone: true, root:"partials" }))
         .pipe(gulp.dest("."));
 });
